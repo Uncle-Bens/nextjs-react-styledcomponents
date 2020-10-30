8317833a0959fb8c0526781743a088851512c1b6
Yevhenii Karachevtsev <eugene.karachevtsev@juriba.com>
2020-10-30 09:42:12 +0200

    export named default

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import Link from 'next/link'
 
-export default () => (
+export default Index = () => (
   <div>
     <Title>React + Styled components</Title>
 
