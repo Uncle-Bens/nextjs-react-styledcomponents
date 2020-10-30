0aaeb42c6927b59fb4b35a7f2d65c001c61b4283
Yevhenii Karachevtsev <eugene.karachevtsev@juriba.com>
2020-10-30 09:00:53 +0200

    added ISR to static page

--- pages/static-props/page.jsx
+++ pages/static-props/page.jsx
@@ -11,12 +11,25 @@ const fetchData = async () => await axios.get('https://jsonplaceholder.typicode.
     }),
   );
 
-const Users = ({ users, error }) => {
+
+function makeid(length) {
+  var result           = '';
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+const Users = ({ users, error, randomId }) => {
   return (
     <section>
       <header>
         <h1>List of users</h1>
       </header>
+      <h2>id: {randomId}</h2>
+      <p>With getStaticProps you don't have to stop relying on dynamic content, as static content can also be dynamic. Incremental Static Regeneration allows you to update existing pages by re-rendering them in the background as traffic comes in.</p>
       {error && <div>There was an error.</div>}
       {!error && users && (
         <table>
@@ -45,8 +58,11 @@ const Users = ({ users, error }) => {
 export const getStaticProps = async () => {
   const data = await fetchData();
 
+  data.randomId = makeid(10);
+
   return {
     props: data,
+    revalidate: 5,
   };
 }
 
