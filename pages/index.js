0a9a2b6dd1feb8556849189d5ec1b273935e538d
Yevhenii Karachevtsev <eugene.karachevtsev@juriba.com>
2020-10-30 09:38:42 +0200

    add navigation

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,32 @@
 import styled from 'styled-components';
+import Link from 'next/link'
 
 export default () => (
   <div>
     <Title>React + Styled components</Title>
+
+    <ul>
+      <li>
+        <Link href="/fetch/page">
+          <a>React App</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/static/page">
+          <a>Static</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/static-props/page">
+          <a>Static Props</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/server-side/page">
+          <a>Server Side</a>
+        </Link>
+      </li>
+    </ul>
   </div>
 );
 
